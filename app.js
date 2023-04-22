bf0bc075dacbcb0b571c7b84d5ef9d8b0f507b06
Harshit Vats <hvats16@gmail.com>
2023-04-22 12:04:33 +0530

    users api

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 
+//Using middleware
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Nice")
-})
+mongoose
+	.connect("mongodb://127.0.0.1:27017", {
+		dbName: "backendapi",
+	})
+	.then(() => console.log("Database Connected"))
+	.catch((e) => console.log(e));
+
+const schema = new mongoose.Schema({
+	name: String,
+	email: String,
+	password: String,
+});
+
+const User = mongoose.model("User", schema);
+
+app.get("/", (req, res) => {
+	res.send("Nice");
+});
+
+app.get("/users/all", async (req, res) => {
+	console.log(req.query);
+	const users = await User.find({});
+	res.json({
+		success: true,
+		users,
+	});
+});
+
+app.post("/users/new", async (req, res) => {
+	const { name, email, password } = req.body;
+	const users = await User.create({
+		name,
+		email,
+		password,
+	});
+	res.status(201).cookie("cookie", "lol").json({
+		success: true,
+		message: "Registered Successfully",
+	});
+});
 
 app.listen("5000", () => {
 	console.log("Listiening on port 5000");
